265c25e1f9b4c780a4ee8bdadf9c599ebbc73d3c
Thomas Smith <tomdsmith1@gmail.com>
2022-11-05 16:21:07 +0000

    Change port number

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   btcd: {
-    uri: 'wss://127.0.0.1:8334/ws',
+    uri: 'wss://127.0.0.1:18334/ws',
     username: 'SVYQJ1hRKsK7d7OZVyJm+20Tecs=',
     password: 'bmECMlFCVaKwtd+T9QwcUvB90JQ=',
     certificatePath: '~/.btcd/rpc.cert'
