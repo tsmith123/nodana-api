3daa6b8e20e119131c58f77e74333b1573dff3c2
Thomas Smith <tomdsmith1@gmail.com>
2022-11-05 15:59:25 +0000

    Update uri for ws

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   btcd: {
-    uri: 'wss://138.68.136.133:8334/ws',
-    username: 'SVYQJ1hRKsK7d7OZVyJm+20Tecs',
+    uri: 'wss://127.0.0.1:8334/ws',
+    username: 'SVYQJ1hRKsK7d7OZVyJm+20Tecs=',
     password: 'bmECMlFCVaKwtd+T9QwcUvB90JQ=',
     certificatePath: '~/.btcd/rpc.cert'
   }
