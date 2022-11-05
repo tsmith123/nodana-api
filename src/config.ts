07465112b1b275be008caebf281c40a5654239a2
Thomas Smith <tomdsmith1@gmail.com>
2022-11-05 16:23:28 +0000

    Remove username and password

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 const config = {
   btcd: {
     uri: 'wss://127.0.0.1:18334/ws',
-    username: 'SVYQJ1hRKsK7d7OZVyJm+20Tecs=',
-    password: 'bmECMlFCVaKwtd+T9QwcUvB90JQ=',
+    username: '',
+    password: '',
     certificatePath: '~/.btcd/rpc.cert'
   }
 };
